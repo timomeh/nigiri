3c420c43fc6da7564d15ad173f75a21c2d979536
Timo Mämecke <timo@maemecke.com>
2018-10-08 01:11:39 +0200

    Add highlight to git progress

--- modules/git.zsh
+++ modules/git.zsh
@@ -1,4 +1,20 @@
-function nigiri_module::git_indicators() {
+function nigiri_module::git() {
+  ! git rev-parse --is-inside-work-tree > /dev/null 2>&1 && return
+
+  local commitish=${$(git symbolic-ref -q HEAD || git name-rev --name-only --no-undefined --always HEAD)#(refs/heads/|tags/)}
+
+  echo -n "%F{black}on %f"
+
+  echo -n "%{%B%}"
+  echo -n "%F{magenta}$commitish%f"
+  echo -n " "
+  nigiri_module::git:indicators
+  nigiri_module::git:progress
+  echo -n "%{%b%}"
+  echo -n " "
+}
+
+function nigiri_module::git:indicators() {
   local indicators
 
   # untracked
@@ -18,40 +34,25 @@ function nigiri_module::git_indicators() {
 
   # detached
   if [[ -n $(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/' | grep detached) ]]; then
-    indicators="🔥 "
+    indicators="🔥"
   fi
 
   # unmodified
   if [ -z "$indicators" ]; then
-    indicators+="%{%F{green}%}■%f"
+    indicators="%{%F{green}%}■%f"
   fi
 
-  echo -n "$indicators "
+  echo -n "$indicators"
 }
 
-function nigiri_module::git_progress() {
+function nigiri_module::git:progress() {
   local git_dir="$(git rev-parse --git-dir 2> /dev/null)"
 
   if test -r "$git_dir/MERGE_HEAD"; then
-    echo -n "(merging)"
+    echo -n "%{%F{red}%}merging%f"
   fi
 
-  if test -d $GIT_DIR/rebase-merge -o -d $GIT_DIR/rebase-apply; then
-    echo -n "(rebasing)"
+  if test -d $git_dir/rebase-merge -o -d $git_dir/rebase-apply; then
+    echo -n "%{%F{red}%}rebasing%f"
   fi
-}
-
-function nigiri_module::git() {
-  ! git rev-parse --is-inside-work-tree > /dev/null 2>&1 && return
-
-  local commitish=${$(git symbolic-ref -q HEAD || git name-rev --name-only --no-undefined --always HEAD)#(refs/heads/|tags/)}
-
-  echo -n "%F{black}on %f"
-
-  echo -n "%{%B%}"
-  echo -n "%F{magenta}$commitish%f"
-  echo -n "%{%b%}"
-  echo -n " "
-  nigiri_module::git_indicators
-  nigiri_module::git_progress
 }
